f6f23b5a7bb3efaeb4a7c33f9eb7bbca11fc5c5b
paulsthorpe <paulsthorpe@gmail.com>
2016-06-13 14:11:54 -0400

    soft deploy

--- app/Category.php
+++ app/Category.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     public function posts() {
+      public $timestamps = false;
       return $this->belongsToMany('App\Post');
     }
 }
